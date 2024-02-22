e1cd0edf2b7c0c7b0bbe2f89cb3895b54e3586ab
Pawel <p.awel608@wp.pl>
2024-02-22 22:47:47 +0100

    working on step4

--- app.js
+++ app.js
@@ -4,15 +4,11 @@
     let bar2 = document.querySelector('#bar2');
     let bar3 = document.querySelector('#bar3');
     let bars = document.querySelector('.bars');
-    let nameLabel = document.querySelector('#nameLabel');
-    let emailLabel = document.querySelector('#emailLabel');
-    let phoneLabel = document.querySelector('#phoneLabel');
     const text1 = document.querySelector('.text1')
     const text2 = document.querySelector('.text2')
     const nameError = document.querySelector('#nameError')
     const emailError = document.querySelector('#emailError')
     const phoneError = document.querySelector('#phoneError')
-    const form = document.querySelector('#form');
     let backButton = document.querySelector(".back-button")
     let nextButton = document.querySelector(".next-button")
     let arcadeButton = document.querySelector(".arcade-button")
@@ -31,7 +27,15 @@
     const advancedPrice = document.querySelector('.advanced-month-price')
     const proPrice = document.querySelector('.pro-month-price')
     const buttons = document.querySelectorAll('.arcade-button, .advanced-button, .pro-button');
-let isButtonClicked = false;
+    let checkboxDisplay = document.querySelector(".only-to-step3")
+    const checkBoxes = document.querySelectorAll(".check-box")
+    let table = document.querySelector(".table")
+    let totalSum = document.querySelector(".total-sum")
+    const finish = document.querySelector(".finish")
+
+
+    let isButtonClicked = false;
+
 
     function checkIfBarsFilled() {
         let isFilled = true;
@@ -58,28 +62,35 @@ let isButtonClicked = false;
         return isFilled;
     }
 
-    let lastClickedButton = true;
+
+
+
 
     function handleClick(event) {
+
         const button = event.currentTarget;
 
-        if (lastClickedButton !== true) {
-            lastClickedButton.style.backgroundColor = '';
+        if (button.style.backgroundColor === "red") {
+
+            button.style.backgroundColor = "";
+            isButtonClicked = false;
         }
+        else {
 
-        button.style.backgroundColor = 'red';
-        lastClickedButton = button;
+            buttons.forEach(btn => btn.style.backgroundColor = "")
+            button.style.backgroundColor = "red";
+            isButtonClicked = true;
 
-
+        }
 
     }
 
 
     function step2() {
 
-        isButtonClicked = false
-
         circleButton1.className = "circle-button1";
+        circleButton3.className = "circle-button3";
+        circleButton4.className = "circle-button4";
         circleButton2.className = 'circle-js';
         backButton.style.display = "block";
         nextButton.style.display = "block";
@@ -90,6 +101,8 @@ let isButtonClicked = false;
         arcadeButton.style.display = "block";
         advancedButton.style.display = "block"
         proButton.style.display = "block"
+        totalSum.style.display = "none";
+        checkboxDisplay.style.display = "none";
 
         toggleCheckbox.addEventListener('change', function () {
             if (this.checked) {
@@ -112,23 +125,58 @@ let isButtonClicked = false;
 
         buttons.forEach(button => {
             button.addEventListener('click', handleClick);
-            isButtonClicked = true;
+
         });
 
 
-        
 
     }
 
+    let isBoxClicked = false;
+    
+    const prices = {}
+
+    const isObjectEmpty = (objectName) => {
+        return Object.keys(objectName).length === 0  //zwraca true or false
+      }
+
     function step3() {
+
+        finish.style.display = "none"
         circleButton2.className = "circle-button2";
+        circleButton4.className = "circle-button4";
         circleButton3.className = 'circle-js';
         backButton.style.display = "block";
         text1.textContent = "Pick add-ons"
         text2.textContent = "Add-ons help enhance your gaming experience."
-        form.style.display = "none";
-        console.log("hello")
-        
+        checkboxDisplay.style.display = "block";
+        arcadeButton.style.display = "none";
+        advancedButton.style.display = "none"
+        proButton.style.display = "none"
+        toggle.style.display = "none";
+        totalSum.style.display = "none";
+        table.style.display = "none";
+        checkBoxes.forEach(checkBox => {
+            checkBox.addEventListener("change", function () {
+                const upPrice = this.parentElement.querySelector('.up-price').textContent
+                const optionTitle = this.parentElement.querySelector('.title').textContent
+                console.log(upPrice)
+                console.log(optionTitle)
+                if (this.checked) {
+                    prices[optionTitle] = upPrice
+                    isBoxClicked = true;
+                }
+                else {
+                    delete prices[optionTitle]
+                    isBoxClicked = false;
+
+                }
+                
+            })
+        })
+
+
+
     }
     function step1() {
         text1.textContent = "Personal info";
@@ -139,28 +187,122 @@ let isButtonClicked = false;
         proButton.style.display = "none"
         circleButton1.className = "circle-js";
         circleButton2.className = "circle-button2";
+        circleButton3.className = "circle-button3";
+        circleButton4.className = "circle-button4";
+        checkboxDisplay.style.display = "none";
         toggle.style.display = "none";
         backButton.style.display = "none";
-        isButtonClicked = true;
+        totalSum.style.display = "none";
+
+
+    }
+
+    const option2Element = document.querySelector('.option2');
+    const price2Element = document.querySelector('.price2');
+
+    function step4() {
+        circleButton4.className = 'circle-js';
+        circleButton1.className = "circle-button1";
+        circleButton2.className = "circle-button2";
+        circleButton3.className = "circle-button3";
+        checkboxDisplay.style.display = "none";
+        text1.textContent = "Finishing up";
+        text2.textContent = "Double-check everything looks OK before confirming."
+        table.style.display = "block";
+        totalSum.style.display = "block";
+        finish.style.display = "none"
+        if (isObjectEmpty(prices)){
+            option2Element.remove()
+            price2Element.remove()
+        }
+        else{
+
+            const togetherElement = document.querySelector('.together');
+            togetherElement.classList.add('together')
+            
+            for (const key in prices) {
+                if (prices.hasOwnProperty(key)) {
+                    
+                    const optionElement = document.createElement('div');
+                    const priceElement = document.createElement('div');
+    
+                    
+                    optionElement.classList.add('option2');
+                    priceElement.classList.add('price2');
+    
+                    optionElement.textContent = key;
+                    priceElement.textContent = prices[key];
+    
+                    
+                    
+                    togetherElement.appendChild(optionElement);
+                    togetherElement.appendChild(priceElement);
+
+                    
+                }
+            }
+        }
+
+    }
+
+
+    function step5() {
+        circleButton4.className = 'circle-js';
+        circleButton1.className = "circle-button1";
+        circleButton2.className = "circle-button2";
+        circleButton3.className = "circle-button3";
+        checkboxDisplay.style.display = "none";
+        text1.textContent = ''
+        text2.textContent = ''
+        table.style.display = "none";
+        totalSum.style.display = "none";
+        finish.style.display = "block"
+        nextButton.style.display = "none";
     }
 
     step1()
 
-    let index = 0;
-    
+    let currentStep = 1;
+
 
     nextButton.addEventListener('click', function () {
-        const isValid = true
- 
-        // checkIfBarsFilled()
-        if (isValid && isButtonClicked) {
-            step2()
+
+
+        if (currentStep === 1) {
+
+            const isStep1Valid = true; //checkIfBarsFilled()
+
+            if (isStep1Valid) {
+                step2()
+                currentStep++;
+                return;
+            }
+        }
+
+        if (currentStep === 2 && isButtonClicked == true) {
             step3()
+            currentStep++;
+            return
         }
-          else{
-            alert('Please choose a plan before going to the next step');
+        // else {
+        //     alert("Please choose a plan before going to the next step") // dziala dla wszystkich
+        // }
+
+        if (currentStep === 3) {                //&& isBoxClicked == true
+            step4()
+            currentStep++;
+            return
         }
-        
+        if (currentStep === 4) {
+            step5()
+            return
+        }
+
+
+
+
+
+
         // if (isButtonClicked == true) {
         //     alert('Please choose a plan you want before going to next step');
         //     return;
@@ -171,29 +313,38 @@ let isButtonClicked = false;
 
 
     })
+
+
     backButton.addEventListener('click', function () {
-        if (bars.value != '') {
-            phoneError.textContent = ''
-            nameError.textContent = ''
-            emailError.textContent = ''
-            bar1.style.borderColor = "hsl(0, 0%, 8%, 0.3)";
-            bar2.style.borderColor = "hsl(0, 0%, 8%, 0.3)";
-            bar3.style.borderColor = "hsl(0, 0%, 8%, 0.3)";
+        if (currentStep === 1) {
+            currentStep++;
+            return
         }
-        if (text1.textContent == "Select your plan") {
-            step1();
+        if (currentStep === 2) {
+            step1()
+            currentStep--;
+            return
         }
+        if (currentStep === 3) {
+            currentStep--;
+            step2()
+            return
+        }
+        if (currentStep === 4) {
+            currentStep--;
+            step3()
+            return
+        }
+        if (currentStep === 5) {
+            currentStep--;
+            step4()
+            return
+        } if (currentStep === 6) {
+            currentStep--;
+            step5()
+            return
+        }
+
     })
-    // form.addEventListener('submit', function (event) {
-    //     event.preventDefault();
-    //     const isValid= checkIfBarsFilled()
-    //     if(isValid){
-    //         step2() // ma byc w srodku, ale po co przeklikiwac za kazdym razem
-    //     }
-
-    //     // step3()
-    // })
-
-
 
 })();
